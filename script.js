77aabf3bef4a9c69bc7e80c7ab77dead278cc80e
Sohee Cho <soheecho94@gmail.com>
2021-04-15 15:16:35 -0400

    commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// function myFunction() {
+//   var x = document.getElementById("myLinks");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "100vw";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
